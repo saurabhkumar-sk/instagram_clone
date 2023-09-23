f7bfb7b715a2742dfc4dc9df4cfa983b3e71448b
saurabhkumar-sk <saurabhkumakairi501@gmail.com>
2023-09-24 03:07:35 +0530

    first commit

---
+++ lib/screens/my_home_page.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
